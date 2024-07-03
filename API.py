b620d4e9004f41e49cd13e884c7b1f49785dc090
zhao-leo <18055219130@163.com>
2024-07-03 17:05:59 +0800

    chore: Update .gitignore to ignore *.tar files and update BASE_URL in API.py

--- API.py
+++ API.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
 
-# BASE_URL=os.getenv('BASE_URL')+'/'
-BASE_URL='http://10.21.235.116:8000/'
+BASE_URL=os.getenv('BASE_URL')+'/'
+# BASE_URL='http://10.21.235.116:8000/'
 # BASE_URL='http://localhost:8000/'
 def loginapi():
   return BASE_URL+'user/ManagerLogin/'
