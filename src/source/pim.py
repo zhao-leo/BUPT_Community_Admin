2acbefa426d666e1ca73e0f4b64bf17b883e8414
zhao-leo <18055219130@163.com>
2024-09-12 22:14:42 +0800

    A little update

--- src/source/pim.py
+++ src/source/pim.py
@@ -21,20 +21,20 @@ class PimPage(PageLayout):
             ui.notify(res.get('message'), type='warning',position='top')
     def content(self):
         with ui.card().style("width:100%;height:100%"):
-            ui.label('欢迎您: {}'.format(app.storage.user.get('NAME'))).style('font-size:1.1rem')
+            ui.label('欢迎您: {}'.format(app.storage.user.get('NAME'))).style('font-size:1.2rem')
             ui.separator()
             with ui.row().style('width:100%'):
-                with ui.card().style('height:350px;width:18%'):
-                    ui.label('以下是您的个人信息:').style('font-size:1.1rem')
+                with ui.card().style('height:350px;width:25%'):
+                    ui.label('以下是您的个人信息:').style('font-size:1.2rem')
                     ui.separator()
-                    ui.label('身份: {}'.format(app.storage.user.get('ROLE'))).style('font-size:1.1rem')
-                    ui.label('电话: {}'.format(app.storage.user.get('PHONE'))).style('font-size:1.1rem')
-                    ui.label('账号: {}'.format(app.storage.user.get('ACCOUNT'))).style('font-size:1.1rem')
+                    ui.label('身份: {}'.format(app.storage.user.get('ROLE'))).style('font-size:1.2rem')
+                    ui.label('电话: {}'.format(app.storage.user.get('PHONE'))).style('font-size:1.2rem')
+                    ui.label('账号: {}'.format(app.storage.user.get('ACCOUNT'))).style('font-size:1.2rem')
 
-                with ui.card().style('height:350px;width:30%'):
+                with ui.card().style('height:350px;width:35%'):
                     ui.label('建议处理情况').style('font-size:1.5rem')
                     ui.echart.from_pyecharts(Pie().add("",list(zip(self.label,self.suggestion))))
-                with ui.card().style('height:350px;width:30%'):
+                with ui.card().style('height:350px;width:35%'):
                     ui.label('诉求处理情况').style('font-size:1.5rem')
                     ui.echart.from_pyecharts(Pie().add("",list(zip(self.label,self.complaint))))
 
