b5b9b61da2b801e3754bce0bf20ed378a390dde7
zhao-leo <18055219130@163.com>
2024-09-18 16:58:22 +0800

    Refactor Dockerfile and build scripts to update image tag and push to registry

--- build.bat
+++ build.bat
@@ -1,1 +1,6 @@
-docker build -t registry.cn-hangzhou.aliyuncs.com/zhao-leo/pythonadmin:2.6.0 -f Dockerfile src/
+docker build -t registry.cn-hangzhou.aliyuncs.com/zhao-leo/pythonadmin:2.6.1 -f Dockerfile src/
+docker tag registry.cn-hangzhou.aliyuncs.com/zhao-leo/pythonadmin:2.6.1 registry.cn-hangzhou.aliyuncs.com/zhao-leo/pythonadmin:latest
+docker login --username=aliyun4438435934 registry.cn-hangzhou.aliyuncs.com
+
+docker push registry.cn-hangzhou.aliyuncs.com/zhao-leo/pythonadmin:2.6.1
+docker push registry.cn-hangzhou.aliyuncs.com/zhao-leo/pythonadmin:latest
